875a23ed88aa3fd101baa3ada93b0349576612df
mengchaoheng <chaohengmeng@163.com>
2023-04-01 08:48:05 +0800

    add read imu baro mag gps csv

--- main/PostEkf.cpp
+++ main/PostEkf.cpp
@@ -41,9 +41,9 @@ void PostEkf::update()
 
        set_px4logstruct((const char**)rowFields);
 
-       _ekf_core.update();
+    //    _ekf_core.update();
 
-       output_csv();
+    //    output_csv();
 
        CsvParser_destroy_row(row);
     }
@@ -67,28 +67,96 @@ void PostEkf::output_csv()
 
 void PostEkf::set_px4logstruct(const char** row_fields)
 {
-    LOGStruct record = {0};
+    // LOGStruct record = {0};
 
-   record.imu_data.time_us      = atoi(row_fields[TIMESTAMP]);
-   record.imu_data.delta_ang(0) = atof(row_fields[GYRO_RAD_X]);
-   record.imu_data.delta_ang(1) = atof(row_fields[GYRO_RAD_Y]);
-   record.imu_data.delta_ang(2) = atof(row_fields[GYRO_RAD_Z]);
-   record.imu_data.delta_ang_dt = atof(row_fields[GYRO_INTEGRAL_DT]);
-   record.imu_data.delta_vel(0) = record.imu_data.delta_ang_dt*atof(row_fields[ACCELEROMETER_M_S2_X]);
-   record.imu_data.delta_vel(1) = record.imu_data.delta_ang_dt*atof(row_fields[ACCELEROMETER_M_S2_Y]);
-   record.imu_data.delta_vel(2) = record.imu_data.delta_ang_dt*atof(row_fields[ACCELEROMETER_M_S2_Z]);
-   record.imu_data.delta_vel_dt = atof(row_fields[ACCELEROMETER_INTEGRAL_DT]);
-   int magnetometer_timestamp_relative = atoi(row_fields[MAGNETOMETER_TIMESTAMP_RELATIVE]);
-   record.mag_data.time_us    = record.imu_data.time_us + magnetometer_timestamp_relative;
-   record.mag_data.mag(0)     = atof(row_fields[MAGNETOMETER_GA_X]);
-   record.mag_data.mag(1)     = atof(row_fields[MAGNETOMETER_GA_Y]);
-   record.mag_data.mag(2)     = atof(row_fields[MAGNETOMETER_GA_Z]);
-   int baro_timestamp_relative = atoi(row_fields[BARO_TIMESTAMP_RELATIVE]);
-   record.baro_data.time_us   = record.imu_data.time_us + baro_timestamp_relative;
-   record.baro_data.hgt       = atof(row_fields[BARO_ALT_METER]);
-   record.baro_temp_celcius   = atof(row_fields[BARO_TEMP_CELCIUS]);
+//    record.imu_data.time_us      = atoi(row_fields[TIMESTAMP]);
+//    record.imu_data.delta_ang(0) = atof(row_fields[GYRO_RAD_X]);
+//    record.imu_data.delta_ang(1) = atof(row_fields[GYRO_RAD_Y]);
+//    record.imu_data.delta_ang(2) = atof(row_fields[GYRO_RAD_Z]);
+//    record.imu_data.delta_ang_dt = atof(row_fields[GYRO_INTEGRAL_DT]);
+//    record.imu_data.delta_vel(0) = record.imu_data.delta_ang_dt*atof(row_fields[ACCELEROMETER_M_S2_X]);
+//    record.imu_data.delta_vel(1) = record.imu_data.delta_ang_dt*atof(row_fields[ACCELEROMETER_M_S2_Y]);
+//    record.imu_data.delta_vel(2) = record.imu_data.delta_ang_dt*atof(row_fields[ACCELEROMETER_M_S2_Z]);
+//    record.imu_data.delta_vel_dt = atof(row_fields[ACCELEROMETER_INTEGRAL_DT]);
+//    int magnetometer_timestamp_relative = atoi(row_fields[MAGNETOMETER_TIMESTAMP_RELATIVE]);
+//    record.mag_data.time_us    = record.imu_data.time_us + magnetometer_timestamp_relative;
+//    record.mag_data.mag(0)     = atof(row_fields[MAGNETOMETER_GA_X]);
+//    record.mag_data.mag(1)     = atof(row_fields[MAGNETOMETER_GA_Y]);
+//    record.mag_data.mag(2)     = atof(row_fields[MAGNETOMETER_GA_Z]);
+//    int baro_timestamp_relative = atoi(row_fields[BARO_TIMESTAMP_RELATIVE]);
+//    record.baro_data.time_us   = record.imu_data.time_us + baro_timestamp_relative;
+//    record.baro_data.hgt       = atof(row_fields[BARO_ALT_METER]);
+//    record.baro_temp_celcius   = atof(row_fields[BARO_TEMP_CELCIUS]);
 
-    _ekf_core.setIMUData( record.imu_data);
-    _ekf_core.setMagData( record.mag_data);
-    _ekf_core.setBaroData(record.baro_data);
+    // printf("[imu]:time %llu, g1 %f, g2 %f, g3 %f, dt %llu,  a1 %f, a2 %f, a3 %f, dt %llu \n", record.imu_data.time_us, record.imu_data.delta_ang(0), record.imu_data.delta_ang(1), record.imu_data.delta_ang(2), record.imu_data.delta_ang_dt, record.imu_data.delta_vel(0), record.imu_data.delta_vel(1), record.imu_data.delta_vel(2), record.imu_data.delta_vel_dt);
+
+
+// user
+
+    // imudata record = {0};
+    // record.timestamp   = atoi(row_fields[TIMESTAMP]);
+    // record.gyro_rad(0) = atof(row_fields[GYRO_RAD_X]);
+    // record.gyro_rad(1) = atof(row_fields[GYRO_RAD_Y]);
+    // record.gyro_rad(2) = atof(row_fields[GYRO_RAD_Z]);
+    // record.gyro_integral_dt  = atoi(row_fields[GYRO_INTEGRAL_DT]);
+    // int32_t magnetometer_timestamp_relative = atoi(row_fields[ACCELEROMETER_TIMESTAMP_RELATIVE]);
+    // record.accelerometer_m_s2(0) = atof(row_fields[ACCELEROMETER_M_S2_X]);
+    // record.accelerometer_m_s2(1) = atof(row_fields[ACCELEROMETER_M_S2_Y]);
+    // record.accelerometer_m_s2(2) = atof(row_fields[ACCELEROMETER_M_S2_Z]);
+    // record.accelerometer_integral_dt = atoi(row_fields[ACCELEROMETER_INTEGRAL_DT]);
+    // printf("[imu]:time %llu, g1 %f, g2 %f, g3 %f, dt %u,  a1 %f, a2 %f, a3 %f, dt %u \n", record.timestamp, record.gyro_rad(0), record.gyro_rad(1), record.gyro_rad(2), record.gyro_integral_dt, record.accelerometer_m_s2(0), record.accelerometer_m_s2(1), record.accelerometer_m_s2(2), record.accelerometer_integral_dt);
+
+
+    // magdata record = {0};
+    // record.timestamp   = atoi(row_fields[TIMESTAMP]);
+    // record.magnetometer_ga(0) = atof(row_fields[MAGNETOMETER_GA_X]);
+    // record.magnetometer_ga(1) = atof(row_fields[MAGNETOMETER_GA_Y]);
+    // record.magnetometer_ga(2) = atof(row_fields[MAGNETOMETER_GA_Z]);
+    // printf("[mag]:time %llu, m1 %f, m2 %f, m3 %f \n", record.timestamp, record.magnetometer_ga(0), record.magnetometer_ga(1), record.magnetometer_ga(2));
+
+    // barodata record = {0};
+    // record.timestamp   = atoi(row_fields[TIMESTAMP]);
+    // record.baro_alt_meter = atof(row_fields[BARO_ALT_METER]);
+    // record.baro_temp_celcius = atof(row_fields[BARO_TEMP_CELCIUS]);
+    // record.baro_pressure_pa = atof(row_fields[BARO_PRESSURE_PA]);
+    // record.rho = atof(row_fields[RHO]);
+    // printf("[baro]:time %llu, baro_alt_meter %f, baro_temp_celcius %f, baro_pressure_pa %f rho %f \n", record.timestamp, record.baro_alt_meter, record.baro_temp_celcius, record.baro_pressure_pa,record.rho);
+
+
+    gpsdata record = {0};
+    record.timestamp   = atoi(row_fields[TIMESTAMP]);
+    record.time_utc_usec   = atoi(row_fields[TIME_UTC_USEC]);
+    record.lat   = atoi(row_fields[LAT]);
+    record.lon   = atoi(row_fields[LON]);
+    record.alt   = atoi(row_fields[ALT]);
+    record.alt_ellipsoid   = atoi(row_fields[ALT_ELLIPSOID]);
+    record.s_variance_m_s = atof(row_fields[S_VARIANCE_M_S]);
+    record.c_variance_rad = atof(row_fields[C_VARIANCE_RAD]);
+    record.eph = atof(row_fields[EPH]);
+    record.epv = atof(row_fields[EPV]);
+    record.hdop = atof(row_fields[HDOP]);
+    record.vdop = atof(row_fields[VDOP]);
+    record.noise_per_ms   = atoi(row_fields[NOISE_PER_MS]);
+    record.jamming_indicator   = atoi(row_fields[JAMMING_INDICATOR]);
+    record.vel_m_s = atof(row_fields[VEL_M_S]);
+    record.vel_n_m_s = atof(row_fields[VEL_N_M_S]);
+    record.vel_e_m_s = atof(row_fields[VEL_E_M_S]);
+    record.vel_d_m_s = atof(row_fields[VEL_D_M_S]);
+    record.cog_rad = atof(row_fields[COG_RAD]);
+    record.timestamp_time_relative   = atoi(row_fields[TIMESTAMP_TIMESTAMP_RELATIVE]);
+    record.fix_type   = atoi(row_fields[FIX_TYPE]);
+    record.vel_ned_valid = (bool) atoi(row_fields[VEL_NED_VALID]);
+    record.satellites_used   = atoi(row_fields[SATELLITES_USED]);
+
+    printf("[gps]: time %llu, time_utc_usec %llu, lat %d, lon %d, alt %d, alt_ellipsoid %d, s_variance_m_s %f, c_variance_rad %f, eph %f, epv %f, hdop %f, vdop %f, noise_per_ms %d, jamming_indicator %d, vel_m_s %f, v1 %f, v2 %f, v3 %f, cog_rad %f, timestamp_time_relative %d, fix_type %d, vel_ned_valid %d, satellites_used %d\n", record.timestamp, record.time_utc_usec, record.lat, record.lon, record.alt, record.alt_ellipsoid, record.s_variance_m_s, record.c_variance_rad, record.eph, record.epv, record.hdop, record.vdop, record.noise_per_ms, record.jamming_indicator, record.vel_m_s, record.vel_n_m_s, record.vel_e_m_s, record.vel_d_m_s, record.cog_rad, record.timestamp_time_relative, record.fix_type, record.vel_ned_valid, record.satellites_used);;
+
+    
+
+
+    
+
+
+    // _ekf_core.setIMUData( record.imu_data);
+    // _ekf_core.setMagData( record.mag_data);
+    // _ekf_core.setBaroData(record.baro_data);
 }
--- main/PostEkf.h
+++ main/PostEkf.h
@@ -15,26 +15,146 @@ enum {
 }
 */
 
-enum {
-   TIMESTAMP,
-   GYRO_RAD_X,
-   GYRO_RAD_Y,
-   GYRO_RAD_Z,
-   GYRO_INTEGRAL_DT,
-   ACCELEROMETER_TIMESTAMP_RELATIVE,
-   ACCELEROMETER_M_S2_X,
-   ACCELEROMETER_M_S2_Y,
-   ACCELEROMETER_M_S2_Z,
-   ACCELEROMETER_INTEGRAL_DT,
-   MAGNETOMETER_TIMESTAMP_RELATIVE,
-   MAGNETOMETER_GA_X,
-   MAGNETOMETER_GA_Y,
-   MAGNETOMETER_GA_Z,
-   BARO_TIMESTAMP_RELATIVE,
-   BARO_ALT_METER,
-   BARO_TEMP_CELCIUS,
-   FIELD_COUNT,
+// enum {
+//    TIMESTAMP,
+//    GYRO_RAD_X,
+//    GYRO_RAD_Y,
+//    GYRO_RAD_Z,
+//    GYRO_INTEGRAL_DT,
+//    ACCELEROMETER_TIMESTAMP_RELATIVE,
+//    ACCELEROMETER_M_S2_X,
+//    ACCELEROMETER_M_S2_Y,
+//    ACCELEROMETER_M_S2_Z,
+//    ACCELEROMETER_INTEGRAL_DT,
+//    MAGNETOMETER_TIMESTAMP_RELATIVE,
+//    MAGNETOMETER_GA_X,
+//    MAGNETOMETER_GA_Y,
+//    MAGNETOMETER_GA_Z,
+//    BARO_TIMESTAMP_RELATIVE,
+//    BARO_ALT_METER,
+//    BARO_TEMP_CELCIUS,
+//    FIELD_COUNT,
+// };
+#define IMU_FIELD_COUNT 10
+#define GPS_FIELD_COUNT 23
+#define MAG_FIELD_COUNT 4
+#define BARO_FIELD_COUNT 5
+
+#define FIELD_COUNT 23
+
+
+#define   TIMESTAMP 0
+#define   GYRO_RAD_X 1
+#define   GYRO_RAD_Y 2
+#define   GYRO_RAD_Z 3
+#define   GYRO_INTEGRAL_DT 4
+#define   ACCELEROMETER_TIMESTAMP_RELATIVE 5
+#define   ACCELEROMETER_M_S2_X 6
+#define   ACCELEROMETER_M_S2_Y 7
+#define   ACCELEROMETER_M_S2_Z 8
+#define   ACCELEROMETER_INTEGRAL_DT 9
+
+
+#define   MAGNETOMETER_GA_X 1
+#define   MAGNETOMETER_GA_Y 2
+#define   MAGNETOMETER_GA_Z 3
+
+#define   BARO_ALT_METER 1
+#define   BARO_TEMP_CELCIUS 2
+#define   BARO_PRESSURE_PA 3
+#define   RHO 4
+
+#define   TIME_UTC_USEC 1
+#define   LAT 2
+#define   LON 3
+#define   ALT 4
+#define   ALT_ELLIPSOID 5
+#define   S_VARIANCE_M_S 6
+#define   C_VARIANCE_RAD 7
+#define   EPH 8
+#define   EPV 9
+#define   HDOP 10
+#define   VDOP 11
+#define   NOISE_PER_MS 12
+#define   JAMMING_INDICATOR 13
+#define   VEL_M_S 14
+#define   VEL_N_M_S 15
+#define   VEL_E_M_S 16
+#define   VEL_D_M_S 17
+#define   COG_RAD 18
+#define   TIMESTAMP_TIMESTAMP_RELATIVE 19
+#define   FIX_TYPE 20
+#define   VEL_NED_VALID 21
+#define   SATELLITES_USED 22
+struct imudata {
+    // // Sensor readings in SI-unit form.
+    // // These fields are scaled and offset-compensated where possible and do not
+    // // change with board revisions and sensor updates.
+    uint64_t timestamp;				// time since system start (microseconds)
+    // // gyro timstamp is equal to the timestamp of the message
+    Vector3f gyro_rad;			// average angular rate measured in the FRD body frame XYZ-axis in rad/s over the last gyro sampling period
+    uint32_t gyro_integral_dt;		// gyro measurement sampling period in microseconds
+
+    int32_t accelerometer_timestamp_relative;	// timestamp + accelerometer_timestamp_relative = Accelerometer timestamp
+    Vector3f accelerometer_m_s2;		// average value acceleration measured in the FRD body frame XYZ-axis in m/s^2 over the last accelerometer sampling period
+    uint32_t accelerometer_integral_dt;	// accelerometer measurement sampling period in microseconds
 };
+
+struct gpsdata {
+	// // GPS position in WGS84 coordinates.
+    // // the auto-generated field 'timestamp' is for the position & velocity (microseconds)
+    uint64_t timestamp;				// time since system start (microseconds)
+    uint64_t time_utc_usec;		// Timestamp (microseconds, UTC), this is the timestamp which comes from the gps module. It might be unavailable right after cold start, indicated by a value of 0 
+    int32_t lat;			// Latitude in 1E-7 degrees
+    int32_t lon;			// Longitude in 1E-7 degrees 
+    int32_t alt;			// Altitude in 1E-3 meters above MSL, (millimetres)
+    int32_t alt_ellipsoid; 		// Altitude in 1E-3 meters bove Ellipsoid, (millimetres)
+
+    float s_variance_m_s;		// GPS speed accuracy estimate, (metres/sec)
+    float c_variance_rad;		// GPS course accuracy estimate, (radians) 
+     
+
+    float eph;			// GPS horizontal position accuracy (metres)
+    float epv;			// GPS vertical position accuracy (metres)
+
+    float hdop;			// Horizontal dilution of precision
+    float vdop;			// Vertical dilution of precision
+
+    int32_t noise_per_ms;		// GPS noise per millisecond
+    int32_t jamming_indicator;		// indicates jamming is occurring
+
+    float vel_m_s;			// GPS ground speed, (metres/sec) 
+    float vel_n_m_s;		// GPS North velocity, (metres/sec) 
+    float vel_e_m_s;		// GPS East velocity, (metres/sec)
+    float vel_d_m_s;		// GPS Down velocity, (metres/sec) 
+    float cog_rad;			// Course over ground (NOT heading, but direction of movement), -PI..PI, (radians) 
+    bool vel_ned_valid;		// True if NED velocity is valid 
+
+    int32_t timestamp_time_relative;	// timestamp + timestamp_time_relative = Time of the UTC timestamp since system start, (microseconds)
+    
+    uint8_t fix_type; // 0-1: no fix, 2: 2D fix, 3: 3D fix, 4: RTCM code differential, 5: Real-Time Kinematic, float, 6: Real-Time Kinematic, fixed, 8: Extrapolated. Some applications will not use the value of this field unless it is at least two, so always correctly fill in the fix.  
+
+    uint8_t satellites_used;		// Number of satellites used 
+};
+
+struct magdata {
+	uint64_t timestamp;            // time since system start (microseconds)
+    uint64_t timestamp_sample;     // the timestamp of the raw data (microseconds)
+    uint32_t device_id;            // unique device ID for the selected magnetometer
+    Vector3f magnetometer_ga;  // Magnetic field in the FRD body frame XYZ-axis in Gauss
+    uint8_t calibration_count;     // Calibration changed counter. Monotonically increases whenever calibration changes.
+};
+
+struct barodata {
+    uint64_t timestamp;            // time since system start (microseconds)
+    uint64_t timestamp_sample;     // the timestamp of the raw data (microseconds)
+	float baro_alt_meter;			// Altitude above MSL calculated from temperature compensated baro sensor data using an ISA corrected for sea level pressure SENS_BARO_QNH.
+    float baro_temp_celcius;		// Temperature in degrees celsius
+    float baro_pressure_pa;		// Absolute pressure in pascals
+
+    float rho;						// air density
+};
+
 
 struct LOGStruct
 {
