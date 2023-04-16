575c020b303e7107825281ff5da10c190e077943
mengchaoheng <chaohengmeng@163.com>
2023-04-16 14:10:03 +0800

    add log data

--- log_data/gen_csv_from_ulg.m
+++ log_data/gen_csv_from_ulg.m
@@ -26,7 +26,7 @@ addpath(genpath(pwd));
 d2r=pi/180;
 r2d=180/pi;
 %% two offline can be use, something different. px4 v12.3.0
-ulgFileName = '17_48_41'; % the ulog file name  17_48_41
+ulgFileName = 'log002'; % the ulog file name  17_48_41
 enable=0;
 tmp1=[ ulgFileName, '_sensor_combined_0.csv'];
 tmp2=[ ulgFileName, '_vehicle_air_data_0.csv'];
